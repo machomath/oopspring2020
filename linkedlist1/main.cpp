0b304274533bc2ff788110148cf51e4d0ff6f356
Dr. Zeb <drzeb@machomath.com>
2020-04-29 16:33:35 -0500

    In class HW

--- linkedlist1/main.cpp
+++ linkedlist1/main.cpp
@@ -8,8 +8,12 @@ int main()
     MyLinkedList linkedList1;
     Student st1("Jose", "CS", 123);
     //st1.printDetails();
-
+    Student st2("Frank", "EE", 127);
+    Student st3("Abraham", "CS", 125);
     linkedList1.insertNode(st1, st1.getKID());
+    linkedList1.insertNode(st2, st2.getKID());
+    linkedList1.insertNode(st3, st3.getKID());
+
     linkedList1.printLinkedList();
     return 0;
 }
--- linkedlist1/src/MyLinkedList.cpp
+++ linkedlist1/src/MyLinkedList.cpp
@@ -12,27 +12,38 @@ MyLinkedList::~MyLinkedList()
 
 void MyLinkedList::insertNode(Student s, int p)
 {
-    cout << "I am coming in the function " << endl;
-    MyNode * tmp;
+    //cout << "I am coming in the function " << endl;
+    MyNode * tmp = new MyNode;
     tmp->stu = s;
     tmp->priority = p;
     tmp->next = nullptr;
     if(head == nullptr){
-        cout << "I am working " << endl;
+        //cout << "I am working " << endl;
         head = tmp;
         tmp = nullptr;
     }else{
-        //work in progress
+        MyNode * current = new MyNode;
+        current = head;
+        while(current->priority < tmp->priority){
+            if(current->next != nullptr){
+                current = current->next;
+            }else{
+                break;
+            }
+        }
+        tmp->next = current->next;
+        current->next = tmp;
     }
 }
 
 void MyLinkedList::printLinkedList()
 {
-    cout << "Our LinkedList is " << endl;
-    //MyNode * tmp = head;
-    //while(tmp != nullptr){
-        //tmp->stu.printDetails();
-        //cout << "---------" << endl;
-        //tmp = tmp->next;
-    //}
+    cout << "Our LinkedList is " << endl
+          << "===============" << endl;
+    MyNode * tmp = head;
+    while(tmp != nullptr){
+        tmp->stu.printDetails();
+        cout << "---------" << endl;
+        tmp = tmp->next;
+    }
 }
